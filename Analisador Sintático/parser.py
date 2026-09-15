5b90f943aac81580da79a3df93e1edd0dfb52d77
FilipeVMoreira <filipe.vllmoreira@gmail.com>
2026-09-15 11:41:44 -0300

    Enhance parser with additional parsing capabilities
    Added parsing functions for identifiers, values, and expressions.

--- Analisador Sintático/parser.py
+++ Analisador Sintático/parser.py
@@ -66,6 +66,88 @@ class Parser:
 
             print(json.dumps(erro, ensure_ascii=False))
             self.teve_erro = True
+
+    def parse_identificador(self):
+        token = self.token_atual()
+
+        if token["token"] == "IDENT":
+            self.avancar()
+        else:
+            erro = {
+                "error": "UNEXPECTED_TOKEN",
+                "lexeme": token["lexeme"],
+                "line": token["line"],
+                "column": token["column"]
+            }
+
+            print(json.dumps(erro, ensure_ascii=False))
+            self.teve_erro = True
+    def parse_valor(self):
+        token = self.token_atual()
+
+        if token["token"] == "INT_LIT":
+            self.avancar()
+
+        elif token["token"] == "FLOAT_LIT":
+            self.avancar()
+
+        else:
+            erro = {
+                "error": "UNEXPECTED_TOKEN",
+                "lexeme": token["lexeme"],
+                "line": token["line"],
+                "column": token["column"]
+            }
+
+            print(json.dumps(erro, ensure_ascii=False))
+            self.teve_erro = True
+
+    def parse_expressao(self):
+        self.parse_expressao_or()
+
+    def parse_expressao_or(self):
+        self.parse_expressao_and()
+
+        while self.token_atual()["token"] == "OR":
+            self.avancar()
+            self.parse_expressao_and()
+
+    def parse_expressao_and(self):
+        self.parse_expressao_igualdade()
+
+        while self.token_atual()["token"] == "AND":
+            self.avancar()
+            self.parse_expressao_igualdade()
+
+    def parse_expressao_igualdade(self):
+        self.parse_expressao_relacional()
+
+        while self.token_atual()["token"] in ["EQ", "NE"]:
+            self.avancar()
+            self.parse_expressao_relacional()
+
+    def parse_expressao_relacional(self):
+        self.parse_expressao_aditiva()
+
+        while self.token_atual()["token"] in ["LT", "GT", "LE", "GE"]:
+            self.avancar()
+            self.parse_expressao_aditiva() 
+
+    def parse_expressao_aditiva(self):
+        self.parse_expressao_multiplicativa()
+
+        while self.token_atual()["token"] in ["PLUS", "MINUS"]:
+            self.avancar()
+            self.parse_expressao_multiplicativa()
+
+    def parse_expressao_multiplicativa(self):
+        self.parse_valor()
+
+        while self.token_atual()["token"] in ["MULT", "DIV", "MOD"]:
+            self.avancar()
+            self.parse_valor()
+
+
     def resultado(self):
         if self.teve_erro:
             return 3
@@ -81,5 +163,13 @@ if __name__ == "__main__":
     parser = Parser(tokens)
 
     parser.parse_tipo()
+
+    parser.parse_identificador()
+
+    if parser.token_atual()["token"] == "ASSIGN":
+        parser.consumir("ASSIGN")
+        parser.parse_expressao()
+
+    parser.consumir("SEMICOLON")
 
     sys.exit(parser.resultado())
